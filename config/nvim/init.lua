9e28676c61ca507c1b9c31d2dc37070f41be7e6d
Dennis Ruppel <crap-dennis@gmx.net>
2025-01-01 03:56:35 +0100

    nvim: fix auto-install of tools

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -561,12 +561,11 @@ require("lazy").setup({
 
 			-- You can add other tools here that you want Mason to install
 			-- for you, so that they are available from within Neovim.
-			local ensure_installed = vim.tbl_keys(servers or {
-				"cpptools",
-				"codelldb",
-			})
+			local ensure_installed = vim.tbl_keys(servers or {})
 			vim.list_extend(ensure_installed, {
 				"stylua",
+				"cpptools",
+				"codelldb",
 			})
 			require("mason-tool-installer").setup({ ensure_installed = ensure_installed })
 
