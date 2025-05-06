a3df1f44ee1e1d2e8e2d7391085f3fab4dec6e27
Dennis Ruppel <crap-dennis@gmx.net>
2025-05-06 18:06:34 +0200

    get snippet movement back

--- config/nvim/lua/plugins/autocomplete.lua
+++ config/nvim/lua/plugins/autocomplete.lua
@@ -3,7 +3,14 @@ return {
 		"saghen/blink.cmp",
 		dependencies = { "rafamadriz/friendly-snippets" },
 		version = "1.*",
-		opts = {},
+		opts = {
+			keymap = {
+				preset = "default",
+				["<C-l>"] = { "snippet_forward", "fallback" },
+				["<C-h>"] = { "snippet_backward", "fallback" },
+			},
+		},
 	},
 }
+
 -- vim: ts=2 sts=2 sw=2 et
